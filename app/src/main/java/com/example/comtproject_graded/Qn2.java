553cf5be403bb5c5829eee4a4b7db0aa32a61fac
Music app <2102415C@student.tp.edu.sg>
2021-05-09 21:32:53 +0800

    v1.1

---
+++ app/src/main/java/com/example/comtproject_graded/Qn2.java
@@ -0,0 +1,9 @@
+package com.example.comtproject_graded;
+
+import java.util.Scanner;
+
+public class Qn2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
