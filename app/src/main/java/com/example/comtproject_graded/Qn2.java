686910103c0129dc47e44fbc784de5f4767930da
Music app <2102415C@student.tp.edu.sg>
2021-05-10 14:55:20 +0800

    v1.2

--- app/src/main/java/com/example/comtproject_graded/Qn2.java
+++ app/src/main/java/com/example/comtproject_graded/Qn2.java
@@ -4,6 +4,19 @@ import java.util.Scanner;
 
 public class Qn2 {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+        //This is for the lower limit
+        System.out.print("Enter lower limit: ");
+        int lowerlimit = input.nextInt();
+        //This is for the upper limit
+        System.out.print("Enter upper limit: ");
+        int upperlimit = input.nextInt();
+        //Counting excluding lower and upper limit
+        System.out.print("Counting: ");
+        for(int i = lowerlimit+1; i<upperlimit; i++){
+            System.out.print(i+" ");
+        }
+
     }
 }
