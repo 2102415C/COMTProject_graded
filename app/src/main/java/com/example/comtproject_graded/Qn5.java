b6a4a0b53aed74c8683ce2411784a31cacf831f3
Music app <2102415C@student.tp.edu.sg>
2021-06-08 21:37:48 +0800

    v1.7

---
+++ app/src/main/java/com/example/comtproject_graded/Qn5.java
@@ -0,0 +1,22 @@
+package com.example.comtproject_graded;
+
+import java.util.Scanner;
+
+public class Qn5 {
+    public static void main(String[] args){
+        System.out.print("Enter first number: ");
+        Scanner firstNumber = new Scanner(System.in);
+        int number1 = firstNumber.nextInt();
+        System.out.print("Enter second number: ");
+        Scanner secondNumber = new Scanner(System.in);
+        int number2 = secondNumber.nextInt();
+        int finalProduct = computeProduct(number1,number2);
+        if (10<number1<50)
+        System.out.println("The product of " + number1 + "and " + number2 + "is " + finalProduct);
+    }
+
+    public static int computeProduct(int firstNumber, int secondNumber) {
+        return firstNumber*secondNumber;
+    }
+
+}
