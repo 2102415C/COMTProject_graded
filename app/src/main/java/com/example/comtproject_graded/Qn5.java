c6f179051872efc225f5e874b0ef0efb1181713d
Music app <2102415C@student.tp.edu.sg>
2021-06-10 18:49:54 +0800

    v1.9

--- app/src/main/java/com/example/comtproject_graded/Qn5.java
+++ app/src/main/java/com/example/comtproject_graded/Qn5.java
@@ -3,23 +3,27 @@ package com.example.comtproject_graded;
 import java.util.Scanner;
 
 public class Qn5 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.print("Enter first number: ");
-        Scanner firstNumber = new Scanner(System.in);
-        int number1 = firstNumber.nextInt();
+        Scanner integer = new Scanner(System.in);
+        //This is to convert the scanner class to int
+        int firstNumber = integer.nextInt();
         System.out.print("Enter second number: ");
-        Scanner secondNumber = new Scanner(System.in);
-        int number2 = secondNumber.nextInt();
-        int finalProduct = computeProduct(number1,number2);
-        if ( number1  number2 ) {
-            System.out.println("The product of " + number1 + "and " + number2 + "is " + finalProduct);
+        integer = new Scanner(System.in);
+        //This is to convert the scanner class to int
+        int secondNumber = integer.nextInt();
+        //To call the method
+        computeProduct(firstNumber, secondNumber);
+    }
+
+    public static void computeProduct(int firstNumber, int secondNumber) {
+        int finalProduct = firstNumber*secondNumber;
+        //To make sure both firstNumber and secondNumber are both in range
+        if ( firstNumber >= 10 && firstNumber <= 50 && secondNumber >= 10 && secondNumber <=50) {
+            System.out.println("The product of " + firstNumber + " and " + secondNumber + " is " + finalProduct);
         }
         else {
             System.out.println("Not in range");
         }
-    }
-
-    public static int computeProduct(int firstNumber, int secondNumber) {
-        return firstNumber*secondNumber;
     }
 }
